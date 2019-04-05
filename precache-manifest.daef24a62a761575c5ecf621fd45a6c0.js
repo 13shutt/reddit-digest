ea3be6fd8b76c73e3dfb91b587be17b3095acb2b
13shutt <shutt13a@gmail.com>
2019-04-05 22:48:39 +0300

    Updates

---
+++ precache-manifest.daef24a62a761575c5ecf621fd45a6c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "86e457761b8f4f9885f4",
+    "url": "https://13shutt.github.io/reddit-digest/static/js/runtime~main.0690f14f.js"
+  },
+  {
+    "revision": "0f5b40ac68614151abfd",
+    "url": "https://13shutt.github.io/reddit-digest/static/js/main.05dab034.chunk.js"
+  },
+  {
+    "revision": "c28c0674672e8dfc77ad",
+    "url": "https://13shutt.github.io/reddit-digest/static/js/2.9f1a9f20.chunk.js"
+  },
+  {
+    "revision": "f217866aa80487cf6241e6f2ee72ce05",
+    "url": "https://13shutt.github.io/reddit-digest/index.html"
+  }
+];
